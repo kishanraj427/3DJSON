88f6316ffd536108927c918dcf7457c67d61d834
Raj Kisan Prasad <raj.kishan@eicoretech.com>
2026-02-05 23:41:26 +0530

    removed import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,6 @@ import {
   Zap,
   AlertCircle,
   CheckCircle,
-  XCircle,
 } from 'lucide-react';
 
 export const Route = createFileRoute('/')({
@@ -225,11 +224,10 @@ function InputScreen() {
                   size="4"
                   disabled={!canVisualize}
                   onClick={handleVisualize}
-                  className={`w-full mt-4 cursor-pointer transition-all duration-200 ${
-                    canVisualize
+                  className={`w-full mt-4 cursor-pointer transition-all duration-200 ${canVisualize
                       ? 'bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700'
                       : 'bg-gray-300 dark:bg-gray-700'
-                  }`}
+                    }`}
                 >
                   <Zap className="w-5 h-5 mr-2" />
                   Visualize in 3D
